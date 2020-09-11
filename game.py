5452342e69d4647bd4ecdb0c42eebce73ad1a90a
julien <jujupoly@orange.fr>
2020-09-11 21:41:23 +0200

    a

--- game.py
+++ game.py
@@ -1,10 +1,12 @@
 import numpy as np
 from tkinter import *
 from random import randrange
-from snake import *
+#from snake import *
 from utils import *
 import pandas as pd
 from model import *
+import time
+TRAINING = True	
 
 class Game():
 	def __init__(self, cell_nb, size):
@@ -20,7 +22,7 @@ class Game():
 		self.apple_exists = False
 		self.apple_x, self.apple_y = self.initApple()
 		self.score = 0
-		print(self.grid, self.snake_x, self.snake_y)
+		#print(self.grid, self.snake_x, self.snake_y)
 		self.moving_left = False
 		self.moving_right = True
 		self.moving_up = False
@@ -51,21 +53,40 @@ class Game():
 
 		for i in range(len(self.snake_x)):
 			grid[self.snake_x[i]][self.snake_y[i]] = i + 1
+
 		return grid
 
 
 
+	def randomBorder(self):
+		randoms_pos = set()
+		for i in range(self.cell_nb):
+			randoms_pos.add((i, 0))
+			randoms_pos.add((0, i))
+			randoms_pos.add((self.cell_nb - 1, i))
+			randoms_pos.add((i, self.cell_nb - 1))
+
+		idx = np.random.choice(len(randoms_pos))
+
+		return list(randoms_pos)[idx]
+
+
+
 	def initSnake(self):
-		x = self.middle * self.size
-		y = self.middle * self.size
+		if TRAINING:
+			idx1, idx2 = self.randomBorder()
+		else:
+			idx1, idx2 = self.middle, self.middle
+
+		x, y =  idx1 * self.size, idx2 * self.size
 		"""
 		self.canvas.bind('<Left>', self.moveLeft)
 		self.canvas.bind('<Right>', self.moveRight)
 		self.canvas.bind('<Up>', self.moveUp)
 		self.canvas.bind('<Down>', self.moveDown)
 		"""
-		self.canvas.create_rectangle(x, y, x + 100, y + 100, fill = "yellow", tag = 'snake_body_0')
-		return [self.middle], [self.middle]
+		self.canvas.create_rectangle(y, x, y + self.size, x + self.size, fill = "yellow", tag = 'snake_body_0')
+		return [idx1], [idx2]
 
 
 
@@ -78,9 +99,9 @@ class Game():
 		canvas_x, canvas_y = x * self.size, y * self.size
 
 		if self.apple_exists:
-			self.canvas.coords('apple', canvas_y, canvas_x, canvas_y + 100 , canvas_x + 100)
+			self.canvas.coords('apple', canvas_y, canvas_x, canvas_y + self.size , canvas_x + self.size)
 		else:
-			self.canvas.create_rectangle(canvas_y, canvas_x, canvas_y + 100 , canvas_x + 100, fill = "red", tag = 'apple')
+			self.canvas.create_rectangle(canvas_y, canvas_x, canvas_y + self.size , canvas_x + self.size, fill = "red", tag = 'apple')
 			self.apple_exists = True
 
 		return x, y
@@ -94,7 +115,7 @@ class Game():
 		y = self.snake_y[-1]
 		tag = f"snake_body_{len(self.snake_x)}"
 		lost = False
-		print(x, y, self.moving_down, self.moving_up, self.moving_right, self.moving_left)
+		#print(x, y, self.moving_down, self.moving_up, self.moving_right, self.moving_left)
 		if not(self.moving_down) and x + 1 < self.cell_nb and self.grid[x + 1][y] == 0:
 			dx = 1
 			dy = 0
@@ -115,7 +136,7 @@ class Game():
 			self.snake_x.append(x + dx)
 			self.snake_y.append(y + dy)
 			canvas_x, canvas_y = (x + dx) * self.size, (y + dy) * self.size
-			self.canvas.create_rectangle(canvas_y, canvas_x, canvas_y + 100 , canvas_x + 100, fill = "green", tag = tag)
+			self.canvas.create_rectangle(canvas_y, canvas_x, canvas_y + self.size , canvas_x + self.size, fill = "green", tag = tag)
 
 
 
@@ -150,11 +171,17 @@ class Game():
 	def updateSnakeBody(self):
 		dx = 0
 		dy = 0
+
 		distance_to_apple = abs(self.apple_x - self.snake_x[0]) + abs(self.apple_y - self.snake_y[0])
-		#self.direction_int = np.random.randint(4)
-		x = np.reshape(self.grid, (1, len(self.grid) ** 2)).flatten()
-		y = np.concatenate((x, [self.score, distance_to_apple]))
-		self.direction_int, direction = predict(model, y)
+		old_score = self.score
+		obstacle_left = 0
+		obstacle_right = 0
+		obstacle_up = 0
+		obstacle_down = 0
+		self.direction_int = np.random.randint(4)
+		#x = np.reshape(self.grid, (1, len(self.grid) ** 2)).flatten()
+		#y = np.concatenate((x, [self.score, distance_to_apple]))
+		#self.direction_int, direction = predict(model, y)
 
 		if self.direction_int == 0:
 			self.moveLeft(None)
@@ -168,8 +195,52 @@ class Game():
 		first_x = self.snake_x[0]
 		first_y  = self.snake_y[0]
 
+		apple_x = self.apple_x
+		apple_y = self.apple_y
+
 		last_x = self.snake_x[-1]
 		last_y = self.snake_y[-1]
+		#print(self.grid)
+
+		vector = [apple_y - first_y, apple_x - first_x]
+		angle = np.angle(np.complex(vector[0], vector[1]), deg = True) / 180
+
+		if (first_y == self.cell_nb - 1) or (self.grid[first_x][first_y + 1] > 0):
+			obstacle_right = 1
+		if (first_y == 0) or (self.grid[first_x][first_y - 1] > 0):
+			obstacle_left = 1
+		if (first_x == 0) or (self.grid[first_x - 1][first_y] > 0):
+			obstacle_up = 1
+		if (first_x == self.cell_nb - 1) or (self.grid[first_x + 1][first_y] > 0):
+			obstacle_down = 1
+
+
+		if not TRAINING:
+			parameters = np.reshape(np.array([obstacle_left, obstacle_right, obstacle_up, obstacle_down, angle, self.direction_int]), (1, 6))
+			prediction = model.predict(parameters).flatten()[0]
+			solution =  0 if prediction <= 0.5 else 1
+
+			print(self.grid)
+			print("left right up down")
+			print([obstacle_left, obstacle_right, obstacle_up, obstacle_down], self.direction_int, prediction, solution)
+
+			while solution == 0:
+				self.direction_int = np.random.randint(4)
+
+				if self.direction_int == 0:
+					self.moveLeft(None)
+				elif self.direction_int == 1:
+					self.moveRight(None)
+				elif self.direction_int == 2:
+					self.moveUp(None)
+				elif self.direction_int == 3:
+					self.moveDown(None)
+
+				parameters = np.reshape(np.array([obstacle_left, obstacle_right, obstacle_up, obstacle_down, angle, self.direction_int]), (1, 6))
+				result = model.predict(parameters).flatten()[0]
+				print(self.direction_int, result)
+				solution =  0 if result <= 0.8 else 1
+
 
 		if self.moving_right and first_y < self.cell_nb - 1 and self.cellIsOkay(first_x, first_y + 1):
 			dx = 0
@@ -191,12 +262,12 @@ class Game():
 				self.snake_y[i] = self.snake_y[i - 1]
 				self.grid[self.snake_x[i]][self.snake_y[i]] = i + 1
 				canvas_x, canvas_y = self.snake_x[i] * self.size, self.snake_y[i] * self.size
-				self.canvas.coords(tag, canvas_y, canvas_x, canvas_y + 100, canvas_x + 100)
+				self.canvas.coords(tag, canvas_y, canvas_x, canvas_y + self.size, canvas_x + self.size)
 
 			self.snake_x[0] += dx
 			self.snake_y[0] += dy
 			canvas_x, canvas_y = self.snake_x[0] * self.size, self.snake_y[0] * self.size
-			self.canvas.coords('snake_body_0', canvas_y, canvas_x, canvas_y + 100, canvas_x + 100)
+			self.canvas.coords('snake_body_0', canvas_y, canvas_x, canvas_y + self.size, canvas_x + self.size)
 
 			tmp = self.grid[first_x + dx][first_y + dy]
 
@@ -206,8 +277,29 @@ class Game():
 			if tmp == -1:
 				self.apple_x, self.apple_y = self.initApple()
 				self.upgradeSnake()
-			#new_distance_to_apple = abs(self.apple_x - self.snake_x[0]) + abs(self.apple_y - self.snake_y[0])
-			print(self.grid, self.snake_x, self.snake_y, direction)#, distance_to_apple, new_distance_to_apple)#, predict(model, self.grid))
+
+			new_distance_to_apple = abs(self.apple_x - self.snake_x[0]) + abs(self.apple_y - self.snake_y[0])
+			out = 1 #1 good move, -1 bad move
+			#print("\n", self.grid)
+			save = np.reshape(np.array([obstacle_left, obstacle_right, obstacle_up, obstacle_down, angle, self.direction_int]), (1, 6))
+			#print(old_score, self.score, new_distance_to_apple, distance_to_apple)
+
+			if old_score > self.score:
+				out = 0
+			elif old_score < self.score:
+				out = 1
+			else:
+				if new_distance_to_apple >= distance_to_apple: #going away from apple or maintaining distance
+					out = 0
+				else:
+					out = 1
+
+			#print("left right up down angle direct out")
+			#print(obstacle_left, obstacle_right, obstacle_up, obstacle_down, angle, self.direction_int, out, model.predict(save))
+			if TRAINING:
+				self.saveMove(obstacle_left, obstacle_right, obstacle_up, obstacle_down, angle, self.direction_int, out)
+			
+			#print(self.grid, self.snake_x, self.snake_y, direction)#, distance_to_apple, new_distance_to_apple)#, predict(model, self.grid))
 			"""
 			if new_distance_to_apple < distance_to_apple:
 				x = np.reshape(self.grid, (1, len(self.grid) ** 2)).flatten()
@@ -224,15 +316,24 @@ class Game():
 				self.canvas.delete(f"snake_body_{i + 1}")
 
 			self.score = 0
-			self.snake_x = [self.middle]
-			self.snake_y = [self.middle]
-			self.canvas.coords("snake_body_0", self.middle * self.size, self.middle * self.size, self.middle * self.size + 100, self.middle * self.size + 100)
+			if TRAINING:
+				idx1, idx2 = self.randomBorder()
+				self.snake_x, self.snake_y = [idx1], [idx2]
+			else:
+				idx1, idx2, = self.middle, self.middle
+				self.snake_x, self.snake_y = [self.middle], [self.middle]
+
+			self.canvas.coords("snake_body_0", idx2 * self.size, idx1 * self.size, idx2 * self.size + self.size, idx1 * self.size + self.size)
 			self.grid = self.createGrid()
 			self.canvas.itemconfigure(self.score_widget, text = "0")
 			self.apple_x, self.apple_y = self.initApple()
-			print(self.grid, self.snake_x, self.snake_y)
+			#print(self.grid, self.snake_x, self.snake_y)
 
-		self.canvas.after(250, self.updateSnakeBody)
+		if TRAINING:
+			self.canvas.after(10, self.updateSnakeBody)
+		else:
+			self.canvas.after(500, self.updateSnakeBody)
+		
 
 
 
@@ -256,9 +357,14 @@ class Game():
 			2 up
 			3 down
 	"""
-	def saveMove(self, to_save):
-		np.savetxt(self.grid_file, to_save, delimiter=';', fmt='%d')
-		np.savetxt(self.move_file, [self.direction_int], delimiter=';', fmt='%d')
+	def saveMove(self, obstacle_left, obstacle_right, obstacle_up, obstacle_down, angle, direction_int, out):
+		to_save = np.reshape(np.array([obstacle_left, obstacle_right, obstacle_up, obstacle_down, angle, direction_int]), (1, 6))
+		#print(to_save, out)
+		np.savetxt(self.grid_file, to_save, delimiter=';', fmt='%f')
+		np.savetxt(self.move_file, [out], delimiter=';', fmt='%d')
 
-model = train()
-x = Game(9, 100)
+if not TRAINING:
+	model = train()
+	x = Game(9, 100)
+else:
+	x = Game(9, 10)	
--- model.py
+++ model.py
@@ -3,38 +3,89 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import numpy as np
 import pandas as pd
 from keras import layers
-from keras.layers import Input, Dense, Activation
+from keras.layers import Input, Dense, Activation, Dropout
 from keras.models import Model, Sequential
 import warnings
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
 def ignore_warn(*args, **kwargs):
     pass
 warnings.warn = ignore_warn #ignore annoying warning (from sklearn and seaborn)
 
 def initModel(input_dim):
 	X = Sequential()
-	X.add(Dense(32, activation = "relu", input_shape = (input_dim, )))
+	X.add(Dense(64, activation = "relu", input_shape = (input_dim, )))
+	X.add(Dropout(0.20))
+	X.add(Dense(64, activation = "relu"))
+	X.add(Dropout(0.20))
+	X.add(Dense(64, activation = "relu"))
+	X.add(Dropout(0.20))
 	X.add(Dense(16, activation = "relu"))
-	X.add(Dense(4, activation = "softmax"))
+	#X.add(Dropout(0.20))
+	X.add(Dense(1, activation = "sigmoid"))
 	return X
 
 def train():
 	x = pd.read_csv('grids.csv', sep = ';', header = None)
 	y = pd.read_csv('moves.csv', sep = ';', header = None)
-	y_one_hot = np.zeros((x.shape[0], 4))
 
-	for i in range(x.shape[0]):
-		y_one_hot[i][y.iloc[i]] = 1
+	m = x.shape[0]
+	parameters_nb = x.shape[1]
 
-	model = initModel(x.shape[1])
-	model.compile(optimizer = "adam", loss	= "categorical_crossentropy", metrics = ['accuracy'])
-	model.fit(x, y_one_hot, batch_size = 32, epochs = 100)
-	return model
+	train_ratio = 0.67
+	test_ratio = 0.33
+	#validation_ratio = 0.10
+
+	x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=1 - train_ratio)
+	#x_val, x_test, y_val, y_test = train_test_split(x_test, y_test, test_size=test_ratio/(test_ratio + validation_ratio))
+
+	model = initModel(parameters_nb)
+	model.compile(optimizer = "adam", loss	= "binary_crossentropy", metrics = ['accuracy'])
+
+	history = model.fit(x_train, y_train, batch_size = 64, epochs = 3)
+	#history = model.fit(pd.concat([x_train, x_test]), pd.concat([y_train, y_test]), validation_split = 0.1, batch_size = 64, epochs = 3)
+	#history = model.fit(x, y, validation_split = 0.33, batch_size = 32, epochs = 10)
+	"""
+	plt.plot(history.history['accuracy'])
+	plt.plot(history.history['val_accuracy'])
+	plt.title('model accuracy')
+	plt.ylabel('accuracy')
+	plt.xlabel('epoch')
+	plt.legend(['train', 'val'], loc='upper left')
+	plt.show()
+
+	plt.plot(history.history['loss'])
+	plt.plot(history.history['val_loss'])
+	plt.title('model loss')
+	plt.ylabel('loss')
+	plt.xlabel('epoch')
+	plt.legend(['train', 'val'], loc='upper left')
+	plt.show()
+	"""
+	predict_y_test = []
+	predictions = model.predict(x_test).flatten()
+	print(m, x_train.shape[0] + x_test.shape[0], x_train.shape[0], x_test.shape[0], len(predictions))
+	error_nb = 0
+	for v in predictions:
+		if v > 0.3:
+			predict_y_test.append(1)
+		else:
+			predict_y_test.append(0)
+
+	for v1, v2 in zip(predict_y_test, y_test):
+		if v1 != v2:
+			error_nb += 1
+
+	print(error_nb)
+	return 0
 
 
 
 def predict(model, x):
-	directions = ["left", "right", "up", "down"]
-	df = pd.DataFrame(data = np.reshape(x, (1, 83)), index = [0], columns=[i for i in range(83)])
+	df = pd.DataFrame(data = np.reshape(x, (1, 6)), index = [0], columns=[i for i in range(6)])
 	predicts = model.predict(df).flatten()
-	max_ind = np.argmax(predicts)
-	return max_ind, directions[max_ind]
+	return predicts
+
+train()
